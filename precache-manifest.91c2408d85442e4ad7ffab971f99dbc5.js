5fed3af2927f17994c05b1d4eaec8f0087df6849
Akram Adib <md.akramuladib2@gmail.com>
2022-07-31 15:23:52 +0600

    Updates

---
+++ precache-manifest.91c2408d85442e4ad7ffab971f99dbc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fbc581268b3556f59c4",
+    "url": "/searchingPeople-updated-/static/css/main.9cec27e0.chunk.css"
+  },
+  {
+    "revision": "3fbc581268b3556f59c4",
+    "url": "/searchingPeople-updated-/static/js/main.3fbc5812.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/searchingPeople-updated-/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "885b144fc3e496518f0f",
+    "url": "/searchingPeople-updated-/static/css/2.264c9d27.chunk.css"
+  },
+  {
+    "revision": "885b144fc3e496518f0f",
+    "url": "/searchingPeople-updated-/static/js/2.885b144f.chunk.js"
+  },
+  {
+    "revision": "72df7f231dca002cbf65",
+    "url": "/searchingPeople-updated-/static/js/runtime~main.72df7f23.js"
+  },
+  {
+    "revision": "b1582fd375fbd282b01b9124def50480",
+    "url": "/searchingPeople-updated-/index.html"
+  }
+];
